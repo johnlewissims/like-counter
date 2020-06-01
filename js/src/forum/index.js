d8bf113bf7f1eab9c3f246f27a71f53c58b24232
johnlewissims <johnsimsemail@gmail.com>
2020-06-01 12:56:48 -0400

    Order Changed

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -10,13 +10,13 @@ app.initializers.add('ejin/like-counter', () => {
   extend(UserCard.prototype, 'infoItems', function (items) {
     const caption = app.forum.attribute('like-counter.caption');
     const likes = this.props.user.data.attributes.ejinLikeCount;
-    items.add('ejin-like-counter', m('span', [icon('fas fa-thumbs-up'), ' ', likes, ' ', caption]));
+    items.add('ejin-like-counter', m('span', likes, ' ', caption, ' ', [icon('fas fa-thumbs-up')]));
   });
 
   extend(CommentPost.prototype, 'headerItems', function (items) {
     if(app.forum.attribute('like-counter.display_under_comments') == 1) {
       const likes = this.props.post.data.attributes.ejinLikeCount;
-      items.add('ejin-like-counter-post', m('span', [icon('fas fa-thumbs-up'), ' ', likes]));
+      items.add('ejin-like-counter-post', m('span', likes, ' ', [icon('fas fa-thumbs-up') ]));
     }
   });
 });
